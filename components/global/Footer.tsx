256b5db0052b6ef04b19189398434e7ee46c95b1
braydentw <reshiramfire130@gmail.com>
2022-07-23 15:15:25 -0500

    Add more fun stuff to Footer

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { footer } from "@/data/global";
+import Image from 'next/image';
 
 function Footer() {
   return (
@@ -66,6 +67,59 @@ function Footer() {
             </p>
           </div>
         </div>
+      </div>
+      <div className="max-w-4xl w-full m-auto mt-8 pt-8 sm:mt-4 sm:pt-4 text-center text-fun-gray border-t border-fun-pink-dark">
+        <p className="flex flex-col items-center justify-center ">
+          <div className="inline-flex items-center uppercase text-xs font-bold tracking-widest">
+            Made with{" "}
+            <div className="space-x-2 inline-flex items-center -mt-1 ml-3">
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                  width="26"
+                  title="React"
+                />
+                <span className="sr-only">React</span>
+              </span>
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
+                  width="40"
+                  className="invert"
+                  title="NextJS"
+                />
+                <span className="sr-only">NextJS</span>
+              </span>
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+                  width="26"
+                  title="TailwindCSS"
+                />
+                <span className="sr-only">TailwindCSS</span>
+              </span>
+            </div>
+          </div>
+          <div className="mt-1">
+            <a
+              href="mailto:brayden45.dev@gmail.com"
+              className="text-white font-bold"
+            >
+              Brayden Wright
+            </a>
+          </div>
+        </p>
+      </div>
+      <div className="mt-8 text-center sm:text-right sm:-mt-12">
+        <a className="w-auto inline-flex items-center sm:w-auto font-bold flex-shrink text-xs border border-fun-pink px-4 py-2 rounded-xl text-fun-pink cursor-pointer opacity-50" href="https://github.com/braydentw/braydentw.io" target="_blank" rel="nooreferrer">
+          <Image
+            src="/static/icons/github.svg"
+            width={16}
+            height={16}
+            alt="Github Icon"
+          />
+          <span className="ml-2">View Source Code </span>
+        </a>
       </div>
     </footer>
   );
