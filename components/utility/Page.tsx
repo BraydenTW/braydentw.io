9e8e91d7529c5126e420786be77b473c826a5fb2
braydentw <reshiramfire130@gmail.com>
2022-08-15 09:14:32 -0500

    Fix GA

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -18,39 +18,77 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
     >
       <Head>
         <title>{pageTitle}</title>
-        <meta
-          name="title"
-          content={pageTitle}
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/favicon/apple-touch-icon.png"
         />
-        <meta
-          name="description"
-          content={desc}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon/favicon-32x32.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/favicon/site.webmanifest" />
+        <meta name="title" content={pageTitle} />
+        <meta name="description" content={desc} />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://braydentw.io/" />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={desc} />
         <meta
-          property="og:title"
-          content={pageTitle}
+          property="og:image"
+          content="https://braydentw.io/static/misc/og.png"
         />
-        <meta
-          property="og:description"
-          content={desc}
-        />
-        <meta property="og:image" content="https://braydentw.io/static/misc/og.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://braydentw.io/" />
+        <meta property="twitter:title" content={pageTitle} />
+        <meta property="twitter:description" content={desc} />
         <meta
-          property="twitter:title"
-          content={pageTitle}
+          property="twitter:image"
+          content="https://braydentw.io/static/misc/og.png"
+        ></meta>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-        <meta
-          property="twitter:description"
-          content={desc}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KC3CN7V');`,
+          }}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
         />
-        <meta property="twitter:image" content="https://braydentw.io/static/misc/og.png"></meta>
       </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC3CN7V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
 
       <main className="p-5 w-full flex-1 text-center">
         <div className="hidden sm:block z-100">
@@ -69,10 +107,10 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
 export default Page;
 
 type PageProps = {
-  currentPage: string,
+  currentPage: string;
   meta: {
-    title?: string,
-    desc: string
-  },
-  children?: JSX.Element | JSX.Element[]
-}
+    title?: string;
+    desc: string;
+  };
+  children?: JSX.Element | JSX.Element[];
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,22 +23,6 @@ class MyDocument extends Document {
             href="/static/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/static/favicon/site.webmanifest" />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body>
           <Main />
